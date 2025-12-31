112a3e1e86bbabf0e743bbc9d41c993e419977a4
mahesh-techvers <mahesh.r@techversantinfotech.com>
2025-12-31 16:19:13 +0530

    Multiple image upload with image texts

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -48,6 +48,9 @@ module.exports.editCampground = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    camp.images.push(...imgs)
+    await camp.save();
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${camp._id}`);
     //res.send("Update Route");
