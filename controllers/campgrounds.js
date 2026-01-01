963f907258c97ad099e265db88f8f9991359befe
mahesh-techvers <mahesh.r@techversantinfotech.com>
2026-01-01 12:57:11 +0530

    update and delete images

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -51,6 +52,15 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
     camp.images.push(...imgs)
     await camp.save();
+    console.log("Request body------>", req.body)
+    console.log("Request delete body------>", req.body.deleteImages)
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await camp.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+        console.log("Campground after delete------>", camp)
+    }
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${camp._id}`);
     //res.send("Update Route");
